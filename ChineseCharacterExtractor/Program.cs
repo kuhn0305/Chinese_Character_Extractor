31f024fc1367e2acd1372355d3fef1d3e5aa7b71
HyunWoo Kang <kuhn0305@gmail.com>
2021-07-01 18:17:19 +0900

    210701 DevLog
    UniCode를 활용하여 문자열 중 한자를 감지한다.

---
+++ ChineseCharacterExtractor/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string s = "姜賢雨";
+
+        char[] charArr = s.ToCharArray();
+
+        foreach (char c in charArr)
+        {
+            if ((c >= 0x4E00 && c <= 0x9FFF))
+            {
+                Console.WriteLine("이것은 한자여!");
+            }
+            else
+            {
+                Console.WriteLine("이것은 아니여!");
+            }
+        }
+
+        Console.Read();
+    }
+}
